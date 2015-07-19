f1407db8711366f46d4be84d822fc800b973ec2c
Lazlo Bonin <lazlo.bonin@gmail.com>
2015-07-19 17:59:12 -0400

    - Added extension method for multiple properties (moved from Ludiq.Reflection) - Small cleanup of PopupGUI

--- Controls/Editor/PopupGUI.cs
+++ Controls/Editor/PopupGUI.cs
@@ -88,17 +88,13 @@ namespace Ludiq.Controls
 
 				menu.DropDown(new Rect(menuPosition, Vector2.zero));
 			}
-			else if (selectedOption != null && !options.Select(o => o.value).Contains(selectedOption.value))
+			else if (selectedOption != null && !options.Select(o => o.value).Contains(selectedOption.value) && !allowOuterOption)
 			{
 				// Selected option isn't in range
 
 				if (hasMultipleDifferentValues)
 				{
 					// Do nothing
-				}
-				if (allowOuterOption)
-				{
-					callback(selectedOption.value);
 				}
 				else if (noneOption != null)
 				{
